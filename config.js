b1a2a25d33c6fbc9bace1b25b8a99707fbaaed14
mohsani-hub <latifa.mohammed.123@icloud.com>
2025-04-18 07:42:05 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id 
+// prince_md-d17e591efa275b017f9a469d74a76d8fadd your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
